576d0a2d7b7b4be4b4c225815825a7d6719525b8
Kaio Delphino <dash.zet@gmail.com>
2026-02-04 14:07:26 -0300

    refactor: move instruction decoder logic to separate function

--- decoder.c
+++ decoder.c
@@ -21,6 +21,71 @@
 
 #define MOV 34
 
+// REG (register) field encoding table
+const char *register_table[16] = {AL, CL, DL, BL, AH, CH, DH, BH,
+                                  AX, CX, DX, BX, SP, BP, SI, DI};
+
+void DecodeInstruction(u_int8_t *instruction_line) {
+  const char *instruction;
+
+  // OPCODE. extract bits 7 to 2 from first byte
+  u_int8_t op_code = (instruction_line[0] & 0xFC) >> 2;
+  switch (op_code) {
+  case MOV:
+    instruction = "mov";
+    break;
+
+  default:
+    printf("This instruction (%d) is not implemented yet!", op_code);
+    exit(1);
+  }
+
+  // check if D bit is set
+  u_int8_t d_bit = (instruction_line[0] & (1 << 2));
+  // printf("D: %d\n", d_bit != 0);
+  (void)d_bit;
+
+  // check if W bit is set
+  u_int8_t w_bit = (instruction_line[0] & 1);
+  // printf("W: %d\n", w_bit != 0);
+  (void)w_bit;
+
+  // MOD. extract bits 7 to 6 from second byte
+  u_int8_t mod = (instruction_line[1] & 0xC0) >> 6;
+  switch (mod) {
+  case 3:
+    break;
+
+  default:
+    printf("This MOD value (%d) is not implemented yet!", mod);
+    exit(1);
+  }
+
+  // R/M. extract bits 2 to 0 from second byte
+  u_int8_t rm = instruction_line[1] & 0x7;
+  switch (rm) {
+  case 56:
+    break;
+
+  default:
+    // MOD is set to Register Mode (11) so R/M identifies
+    // the second register operand.
+    if (mod == 3) {
+      break;
+    }
+    printf("This R/M value (%d) is not implemented yet!", rm);
+    exit(1);
+  }
+
+  // REG. extract bits 5 to 3 from second byte
+  u_int8_t reg = (instruction_line[1] & 0x38) >> 3;
+  const char *source_register = register_table[(w_bit << 3) | reg];
+  const char *destination_register = register_table[(w_bit << 3) | rm];
+
+  printf("bits 16\n\n");
+  printf("%s %s, %s\n", instruction, destination_register, source_register);
+}
+
 int main(int argc, char **argv) {
   (void)argc;
   if (!argv[1]) {
@@ -37,6 +102,7 @@ int main(int argc, char **argv) {
   // calculate the file size
   fseek(file_ptr, 0, SEEK_END);
   long file_length = ftell(file_ptr);
+  printf("[debug] file length: %ld\n", file_length);
   rewind(file_ptr);
 
   // allocate and copy the file contents
@@ -44,83 +110,20 @@ int main(int argc, char **argv) {
   fread(buffer, file_length, 1, file_ptr);
   fclose(file_ptr);
 
-  // // print file content as bytes
-  // printf("%d %d\n", buffer[0], buffer[1]);
-  // // print file content bits
-  // printf("00000000: ");
-  // for (int i = 7; i >= 0; i--) {
-  //   printf("%d", (buffer[0] & (1 << i)) != 0);
-  // }
-  // printf(" ");
-  // for (int i = 7; i >= 0; i--) {
-  //   printf("%d", (buffer[0] & (1 << i)) != 0);
-  // }
-  // printf("\n");
-
-  // REG (register) field encoding table
-  const char *register_table[16] = {AL, CL, DL, BL, AH, CH, DH, BH,
-                                    AX, CX, DX, BX, SP, BP, SI, DI};
-  // 10001001 11011001
-  (void)register_table;
-
-  const char *instruction;
-
-  // OPCODE. extract bits 7 to 2 from first byte
-  u_int8_t op_code = (buffer[0] & 0xFC) >> 2;
-  switch (op_code) {
-  case MOV:
-    instruction = "mov";
-    break;
-
-  default:
-    printf("This instruction (%d) is not implemented yet!", op_code);
-    exit(1);
+  // print file content as bytes
+  printf("%d %d\n", buffer[0], buffer[1]);
+  // print file content bits
+  printf("00000000: ");
+  for (int i = 7; i >= 0; i--) {
+    printf("%d", (buffer[0] & (1 << i)) != 0);
   }
-
-  // check if D bit is set
-  u_int8_t d_bit = (buffer[0] & (1 << 2));
-  // printf("D: %d\n", d_bit != 0);
-  (void)d_bit;
-
-  // check if W bit is set
-  u_int8_t w_bit = (buffer[0] & 1);
-  // printf("W: %d\n", w_bit != 0);
-  (void)w_bit;
-
-  // MOD. extract bits 7 to 6 from second byte
-  u_int8_t mod = (buffer[1] & 0xC0) >> 6;
-  switch (mod) {
-  case 3:
-    break;
-
-  default:
-    printf("This MOD value (%d) is not implemented yet!", mod);
-    exit(1);
+  printf(" ");
+  for (int i = 7; i >= 0; i--) {
+    printf("%d", (buffer[0] & (1 << i)) != 0);
   }
+  printf("\n");
 
-  // R/M. extract bits 2 to 0 from second byte
-  u_int8_t rm = buffer[1] & 0x7;
-  switch (rm) {
-  case 56:
-    break;
-
-  default:
-    // MOD is set to Register Mode (11) so R/M identifies
-    // the second register operand.
-    if (mod == 3) {
-      break;
-    }
-    printf("This R/M value (%d) is not implemented yet!", rm);
-    exit(1);
-  }
-
-  // REG. extract bits 5 to 3 from second byte
-  u_int8_t reg = (buffer[1] & 0x38) >> 3;
-  const char *source_register = register_table[(w_bit << 3) | reg];
-  const char *destination_register = register_table[(w_bit << 3) | rm];
-
-  printf("bits 16\n\n");
-  printf("%s %s, %s\n", instruction, destination_register, source_register);
+  DecodeInstruction(buffer);
 
   return 0;
 }
