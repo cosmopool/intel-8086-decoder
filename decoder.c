9351a5b7f08956b5814d106603f5bd1478a4bf9e
Kaio Delphino <dash.zet@gmail.com>
2026-02-04 14:23:37 -0300

    feat: read multiple instructions

--- decoder.c
+++ decoder.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -25,11 +26,13 @@
 const char *register_table[16] = {AL, CL, DL, BL, AH, CH, DH, BH,
                                   AX, CX, DX, BX, SP, BP, SI, DI};
 
-void DecodeInstruction(u_int8_t *instruction_line) {
+// Returns how many bytes has read
+int DecodeInstruction(u_int8_t *bytes) {
   const char *instruction;
+  int cursor = 0;
 
   // OPCODE. extract bits 7 to 2 from first byte
-  u_int8_t op_code = (instruction_line[0] & 0xFC) >> 2;
+  u_int8_t op_code = (bytes[cursor] & 0xFC) >> 2;
   switch (op_code) {
   case MOV:
     instruction = "mov";
@@ -41,17 +44,18 @@ void DecodeInstruction(u_int8_t *instruction_line) {
   }
 
   // check if D bit is set
-  u_int8_t d_bit = (instruction_line[0] & (1 << 2));
+  u_int8_t d_bit = (bytes[cursor] & (1 << 2));
   // printf("D: %d\n", d_bit != 0);
   (void)d_bit;
 
   // check if W bit is set
-  u_int8_t w_bit = (instruction_line[0] & 1);
+  u_int8_t w_bit = (bytes[cursor] & 1);
   // printf("W: %d\n", w_bit != 0);
   (void)w_bit;
 
+  cursor += 1;
   // MOD. extract bits 7 to 6 from second byte
-  u_int8_t mod = (instruction_line[1] & 0xC0) >> 6;
+  u_int8_t mod = (bytes[cursor] & 0xC0) >> 6;
   switch (mod) {
   case 3:
     break;
@@ -62,7 +66,7 @@ void DecodeInstruction(u_int8_t *instruction_line) {
   }
 
   // R/M. extract bits 2 to 0 from second byte
-  u_int8_t rm = instruction_line[1] & 0x7;
+  u_int8_t rm = bytes[cursor] & 0x7;
   switch (rm) {
   case 56:
     break;
@@ -78,12 +82,13 @@ void DecodeInstruction(u_int8_t *instruction_line) {
   }
 
   // REG. extract bits 5 to 3 from second byte
-  u_int8_t reg = (instruction_line[1] & 0x38) >> 3;
+  u_int8_t reg = (bytes[cursor] & 0x38) >> 3;
   const char *source_register = register_table[(w_bit << 3) | reg];
   const char *destination_register = register_table[(w_bit << 3) | rm];
 
-  printf("bits 16\n");
   printf("%s %s, %s\n", instruction, destination_register, source_register);
+
+  return cursor;
 }
 
 int main(int argc, char **argv) {
@@ -123,7 +128,15 @@ int main(int argc, char **argv) {
   }
   printf("\n\n");
 
-  DecodeInstruction(buffer);
+  printf("bits 16\n");
+  int instruction_start = 0;
+  while (instruction_start < file_length) {
+    // last instruction read
+    instruction_start += DecodeInstruction(buffer + instruction_start);
+    assert(buffer[instruction_start] != '\0');
+    // start at the next one
+    instruction_start += 1;
+  }
 
   return 0;
 }
