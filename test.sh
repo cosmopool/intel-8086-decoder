413628767ec21f3451aebd3df28d12a47c05c736
Kaio Delphino <dash.zet@gmail.com>
2026-02-06 17:24:03 -0300

    feat: add cleanup file after finish testing

--- test.sh
+++ test.sh
@@ -6,30 +6,41 @@ RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 NORMAL=$(tput sgr0)
 
-for LISTING_ASM in listing_*.asm; do
-  LISTING="${LISTING_ASM%.*}"
-  printf "testing: %s: " "$LISTING"
+diff_asm_file_for_listing() {
+  awk NF "$1" | awk '!/^[[:blank:]]*;/' | diff --color "decoded_$1" -
+}
 
-  # compile binary for this listing if does not exists
-  if [ ! -e "$LISTING" ]; then
-    nasm "$LISTING"
-  fi
+test_all() {
+  for LISTING_ASM in listing_*.asm; do
+    LISTING="${LISTING_ASM%.*}"
+    printf "testing: %s: " "$LISTING"
+  
+    # compile binary for this listing if does not exists
+    if [ ! -e "$LISTING" ]; then
+      nasm "$LISTING"
+    fi
+  
+    if ! ./decoder "$LISTING" > "decoded_$LISTING_ASM"; then
+      printf "%s\n" "${RED}decoding failed${NORMAL}"
+      diff_asm_file_for_listing "$LISTING_ASM"
+      return 1
+    fi
+  
+    if ! nasm "decoded_$LISTING_ASM"; then
+      printf "%s\n" "${RED}compiling failed${NORMAL}"
+      diff_asm_file_for_listing "$LISTING_ASM"
+      return 1
+    fi
+  
+    if  diff "$LISTING" "decoded_$LISTING" 1> /dev/null; then 
+      printf "%s\n" "${GREEN}pass${NORMAL}"
+    else
+      printf "%s\n" "${RED}failed${NORMAL}"
+      diff_asm_file_for_listing "$LISTING_ASM"
+      return 1
+    fi
+  done
+}
 
-  if ! ./decoder "$LISTING" > "decoded_$LISTING_ASM"; then
-    printf "%s\n" "${RED}decoding failed${NORMAL}"
-    exit 1
-  fi
-
-  if ! nasm "decoded_$LISTING_ASM"; then
-    printf "%s\n" "${RED}compiling failed${NORMAL}"
-    exit 1
-  fi
-
-
-  if  diff "$LISTING" "decoded_$LISTING" 1> /dev/null; then 
-    printf "%s\n" "${GREEN}pass${NORMAL}"
-  else
-    printf "%s\n" "${RED}failed${NORMAL}"
-    exit 1
-  fi
-done
+test_all
+rm -rf decoded_listing_*
